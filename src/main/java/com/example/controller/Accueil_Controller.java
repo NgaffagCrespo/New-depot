d2ba9032ea05fe01dc9ba65a2fb93fe3be8f3e67
silatcha <silatchayvan@gmail.com>
2020-12-29 03:31:23 +0100

    base de donnee

--- src/main/java/com/example/controller/Accueil_Controller.java
+++ src/main/java/com/example/controller/Accueil_Controller.java
@@ -1,14 +1,38 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@Controller
 public class Accueil_Controller {
 
 	
-	
-	@GetMapping("/")
+	@GetMapping({"/","/index"})
 	public String home(){
 	    return "index";
 
 	}
+
+
+	
+	@RequestMapping("/welcome")
+	public String welcom(){
+	    return "welcompage";
+	}
+	
+	
+	@GetMapping("/home")
+	public String home1(){
+	    return "home";
+
+	}
+	
+	@GetMapping("/pages/home")
+	public String topPage(){
+	    return "/pages/home";
+	}
+	
 }
